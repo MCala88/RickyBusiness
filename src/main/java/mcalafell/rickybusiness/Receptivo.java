e0a6e8e1f05e5e5bd20c79e0f7bf0d02ab3072f8
Miquel Calafell <56043308+MCala88@users.noreply.github.com>
2020-02-26 13:10:39 +0100

    refact 2 recept

--- src/main/java/mcalafell/rickybusiness/Receptivo.java
+++ src/main/java/mcalafell/rickybusiness/Receptivo.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Receptivo implements GuestDispatcher {
 	ArrayList<GuestDispatcher> guest = new ArrayList<GuestDispatcher>();
 	
-	public Receptivo() {};
+
 	
 	public void registra(GuestDispatcher despachador) {
 		this.guest.add(despachador);
