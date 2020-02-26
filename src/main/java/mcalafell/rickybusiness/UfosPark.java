12c5cf7f9a2bd86866a0b366e00bab157718a81b
Miquel Calafell <56043308+MCala88@users.noreply.github.com>
2020-02-26 13:07:53 +0100

    refact ufospark

--- src/main/java/mcalafell/rickybusiness/UfosPark.java
+++ src/main/java/mcalafell/rickybusiness/UfosPark.java
@@ -2,49 +2,44 @@ package mcalafell.rickybusiness;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class UfosPark {
+public class UfosPark implements GuestDispatcher {
 	private double fee = 500;
-	static ArrayList<String> nombresFlotas = new ArrayList <String>();
-	static ArrayList<String> flota = new ArrayList <String>();
-	private Map<String, String> flotaReservada = new HashMap <String, String>();
-	
-	public UfosPark() {
-	}
+	static List<String> nombresFlotas = new ArrayList <String>();
+	static List<String> flota = new ArrayList <String>();
+	private Map<String, String> flotasReservadas = new HashMap <String, String>();
 	
 	public void add(String añadir) {
 		flota.add(añadir);
 		nombresFlotas.add(añadir);
 	}
 	
-	public Map<String, String> getFlotaReserva(){
-		return flotaReservada;
+	public Map<String, String> getFlotasReservadas(){
+		return flotasReservadas;
 	}
 	
-	public ArrayList<String> getFlota(){
+	public List<String> getFlota(){
 		return flota;
 	}
 	
 	public void dispatch(CreditCard tarjeta) {
-		if (this.getFlotaReserva().containsKey(tarjeta.number())) {
+		if (this.getFlotasReservadas().containsKey(tarjeta.number())) {
 			
-		}
-		else {
-			if (tarjeta.credit() >= fee & flota.size() > 0 ) {
+		} else {
+			if (tarjeta.credit() >= fee && flota.size() > 0 ) {
 				tarjeta.pay(fee);
-				this.flotaReservada.put(tarjeta.number(), flota.remove(0));
+				this.flotasReservadas.put(tarjeta.number(), flota.remove(0));
 			}
 		}
-		
 	}
 		
-	
 
 	public String getUfoOf(String propietario) {
-		return flotaReservada.get(propietario);
+		return flotasReservadas.get(propietario);
 	}
 	public String toString()  { 
-        return nombresFlotas.toString() ; 
+        return nombresFlotas.toString(); 
     }
 }
